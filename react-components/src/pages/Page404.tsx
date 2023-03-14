9c38da55951cac712368be84eb584590bb2dc7ad
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-14 20:21:07 +0300

    feat: content for 404 page

--- react-components/src/pages/Page404.tsx
+++ react-components/src/pages/Page404.tsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Page404 extends Component {
   render() {
-    return <div>Page404</div>;
+    return (
+      <div className="page-404">
+        <div className="container">
+          <h1 className="page-404__title">404 Not found</h1>
+          <p className="page-404__text">We couldn&apos;t find this page :(</p>
+          <Link className="page-404__link" to="/">
+            Back home
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
