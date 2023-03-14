7d0ecdebc34ad56247ae20e194c2b593f7d27c2a
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-14 14:29:18 +0300

    feat: add searchbar component

--- react-components/src/pages/Home.tsx
+++ react-components/src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from '../components/SearchBar';
 
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="home">
+        <div className="container">
+          <SearchBar />
+        </div>
+      </div>
+    );
   }
 }
