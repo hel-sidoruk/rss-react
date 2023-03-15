c8bc3b1077601b6737cd67934b5b50e733b8eb77
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-15 22:49:21 +0300

    feat: implement dropdown component

--- react-components/src/components/Dropdown/Dropdown.tsx
+++ react-components/src/components/Dropdown/Dropdown.tsx
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
 import styles from './dropdown.module.scss';
 
-export class Dropdown extends Component {
+const tags = ['Graphics', 'Design', 'Inspiration', 'Art', 'Modern', 'Architecture', 'Vintage'];
+
+type State = { tags: string[]; isOpened: boolean };
+
+export class Dropdown extends Component<object, State> {
+  state: State = { tags: [], isOpened: false };
+
+  constructor(props: object) {
+    super(props);
+    this.changeTags = this.changeTags.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  componentDidMount() {
+    document.body.addEventListener('click', this.close);
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('click', this.close);
+  }
+
+  close() {
+    this.setState({ isOpened: false });
+  }
+
+  changeTags(e: React.MouseEvent<HTMLLIElement>, tag: string) {
+    e.stopPropagation();
+    this.setState(({ tags }) => ({
+      tags: tags.includes(tag) ? tags.filter((el) => el !== tag) : [...tags, tag],
+    }));
+  }
+
+  toggleOpen(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+    this.setState(({ isOpened }) => ({ isOpened: !isOpened }));
+  }
+
   render() {
-    return <div>Dropdown</div>;
+    return (
+      <div className={styles.dropdown}>
+        <div
+          className={`${styles.top} ${this.state.isOpened ? styles.opened : ''}`}
+          onClick={this.toggleOpen}
+        >
+          {this.state.tags.length
+            ? this.state.tags.map((el) => (
+                <div key={el + '1'} className={styles.tag}>
+                  {el}
+                </div>
+              ))
+            : 'Select tags'}
+        </div>
+        <ul className={`${styles.bottom} ${this.state.isOpened ? styles.active : ''}`}>
+          {tags.map((tag) => (
+            <li
+              key={tag}
+              className={`${styles.item} ${this.state.tags.includes(tag) ? styles.marked : ''}`}
+              onClick={(e) => this.changeTags(e, tag)}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
