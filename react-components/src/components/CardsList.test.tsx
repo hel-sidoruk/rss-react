a043ea058e081361f75f2deb6babadb51812a766
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-15 20:27:08 +0300

    feat: add tests for CardsList component

---
+++ react-components/src/components/CardsList.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import CardsList from './CardsList';
+
+test('CardsList renders cards', () => {
+  render(<CardsList size={6} />);
+  const cards = document.querySelectorAll('.card');
+  expect(cards.length).toBe(6);
+});
