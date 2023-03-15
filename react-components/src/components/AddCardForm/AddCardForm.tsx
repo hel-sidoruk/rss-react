0d5a84de8f65bb2c8e164c4a73445c39d26d3e3b
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-15 21:23:56 +0300

    feat: add basic form structure

--- react-components/src/components/AddCardForm/AddCardForm.tsx
+++ react-components/src/components/AddCardForm/AddCardForm.tsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { Dropdown } from '../Dropdown';
 
 export class AddCardForm extends Component {
   render() {
-    return <div>AddCardForm</div>;
+    return (
+      <form>
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" placeholder="Title" />
+        <label htmlFor="author">Your name</label>
+        <input id="author" type="text" placeholder="Your name" />
+        <label htmlFor="description">Description</label>
+        <textarea id="description" placeholder="Description" />
+        <input type="file" />
+        <Dropdown />
+      </form>
+    );
   }
 }
