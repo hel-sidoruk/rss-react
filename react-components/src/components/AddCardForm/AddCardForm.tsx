c35c521378c07a23bc4a3e13f7f635e6ff24ec93
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-22 22:13:40 +0300

    feat: add react-hook-form

--- react-components/src/components/AddCardForm/AddCardForm.tsx
+++ react-components/src/components/AddCardForm/AddCardForm.tsx
@@ -1,59 +1,91 @@
-import React, { useRef, useState } from 'react';
-import { Form } from './Form';
+import { useState } from 'react';
 import { ErrorsState, IPost } from '../../types';
 import { changeTags, initialErrors, randomId } from '../../utils';
-import { validate } from '../../utils/validation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import styles from './form.module.scss';
+import { DropdownField } from './DropdownField';
+import { FileInput } from './FileInput';
 
 type Props = { addPost: (post: IPost) => void };
 
+interface IForm {
+  text: string;
+  date: string;
+  gender: 'Male' | 'Female';
+  check: boolean;
+}
+
 export const AddCardForm = ({ addPost }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IForm>();
   const [tags, setTags] = useState<string[]>([]);
   const [success, setSuccess] = useState(false);
-  const [errors, setErrors] = useState<ErrorsState>({ ...initialErrors });
+  const [myErrors, setMyErrors] = useState<ErrorsState>({ ...initialErrors });
   const [image, setImage] = useState<string | ArrayBuffer>('');
-  const formRef = useRef<HTMLFormElement>(null);
 
   const tagsChange = (tag: string) => setTags((tags) => changeTags(tags, tag));
 
-  const reset = () => {
-    formRef.current?.reset();
+  const resetForm = () => {
+    reset();
     setTags([]);
     setImage('');
   };
 
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit: SubmitHandler<IForm> = (data) => {
     const post: IPost = {
       id: randomId(),
-      text: formRef.current?.['text'].value,
-      date: formRef.current?.['date'].value,
+      ...data,
       image: image as string,
       tags,
-      gender: formRef.current?.['female'].checked
-        ? 'Female'
-        : formRef.current?.['male'].checked
-        ? 'Male'
-        : '',
     };
-    const newErrors = validate(post, formRef.current?.['check'].checked);
-    setErrors(newErrors);
-    if (Object.values(newErrors).some(Boolean)) return;
+    resetForm();
     addPost(post);
-    reset();
     setSuccess(true);
     setTimeout(() => setSuccess(false), 2000);
   };
 
   return (
-    <Form
-      success={success}
-      formRef={formRef}
-      errors={errors}
-      onSubmit={onSubmit}
-      tags={tags}
-      changeTags={tagsChange}
-      image={image}
-      changeImage={setImage}
-    />
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.field}>
+        <input
+          className={styles.input}
+          placeholder="Title"
+          {...register('text', { required: true })}
+        />
+        {errors.text && <p>The title is required</p>}
+      </div>
+      <div className={styles.field}>
+        <input className={styles.input} type="date" {...register('date', { required: true })} />
+        {errors.date && <p>The date is required</p>}
+      </div>
+      <div className={styles.field}>
+        <FileInput changeImage={setImage} image={image} />
+        <p>{myErrors.image}</p>
+      </div>
+      <DropdownField error={myErrors.tags} change={tagsChange} tags={tags} />
+      <div className={styles.field}>
+        <input
+          {...register('gender', { required: true })}
+          id="female"
+          value="Female"
+          type="radio"
+        />
+        <label htmlFor="female">Female</label>
+        <input {...register('gender', { required: true })} id="male" value="Male" type="radio" />
+        <label htmlFor="male">Male</label>
+        {errors.gender && <p>Please, select your gender</p>}
+      </div>
+      <div className={styles.field}>
+        <input id="check" type="checkbox" {...register('check', { required: true })} />
+        <label htmlFor="check">I agree to publish this data</label>
+        {errors.check && <p>You should agree to post this</p>}
+      </div>
+      <button className={styles.btn}>Create</button>
+      {success && <div className={styles.success}>The data has been saved successfully</div>}
+    </form>
   );
 };
