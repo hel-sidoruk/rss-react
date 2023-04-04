99dfdb48d296f467089f845ab359fec61d5bd946
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-04-04 09:20:44 +0300

    fix: save input value in local storage

--- react-components/src/components/SearchBar/SearchBar.tsx
+++ react-components/src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SearchIcon } from '../../Icons';
 import styles from './searchbar.module.scss';
 
 export const SearchBar = () => {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+  const [value, setValue] = useState(localStorage.getItem('searchValue') || '');
+  const valueRef = useRef<string>('');
 
   useEffect(() => {
-    const searchbarValue = localStorage.getItem('searchbarValue');
-    if (searchbarValue) setValue(searchbarValue);
-    return () => localStorage.setItem('searchbarValue', value);
+    return () => {
+      localStorage.setItem('searchValue', valueRef.current);
+    };
   }, []);
+
+  useEffect(() => {
+    valueRef.current = value;
+  }, [value]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
 
   return (
     <div className={styles.searchbar}>
