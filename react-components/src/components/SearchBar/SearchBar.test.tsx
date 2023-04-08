479dc0daf3740ce41e476ac63fe8041151283c32
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-04-08 20:13:41 +0300

    fix: fix test

--- react-components/src/components/SearchBar/SearchBar.test.tsx
+++ react-components/src/components/SearchBar/SearchBar.test.tsx
@@ -2,13 +2,13 @@ import { fireEvent, render } from '@testing-library/react';
 import { SearchBar } from './SearchBar';
 
 test('There is a searchbar on the page', () => {
-  const { getByPlaceholderText } = render(<SearchBar />);
+  const { getByPlaceholderText } = render(<SearchBar onSubmit={() => {}} />);
   const input = getByPlaceholderText(/Search/);
   expect(input).toBeInTheDocument();
 });
 
 test('Searchbar value is changing on input', () => {
-  const { getByPlaceholderText } = render(<SearchBar />);
+  const { getByPlaceholderText } = render(<SearchBar onSubmit={() => {}} />);
   const input = getByPlaceholderText(/Search/);
   fireEvent.change(input, { target: { value: 'a' } });
   expect((input as HTMLInputElement).value).toBe('a');
